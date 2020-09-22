bdfea01d0191cde46167bd176f1cb7e160d2673d
German Yakimov <german13yakimov@gmail.com>
2020-09-22 12:17:08 +0700

    Delete task2.

--- task2/task2/task2/gauss.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-
-double** upper_triangle(double** matrix, size_t height, size_t width);
-
-double* express_variables(double** matrix, size_t height, size_t width);
-
