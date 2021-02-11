a33a91b293c1cb0860357610c96a23cac6c07963
sterrestelsel <toscha.nv@gmail.com>
2021-02-11 11:56:18 +0700

    Fixed sor method

--- term_2/task_1/main.py
+++ term_2/task_1/main.py
@@ -1,5 +1,7 @@
 import math
+import builtins
 
+golden = (1 + 5 ** 0.5) / 2
 
 def bisections(func, a, b, epsilon):
     cnt = 0
@@ -22,9 +24,54 @@ def bisections(func, a, b, epsilon):
 def derivative(func, x, epsilon):
     return (func(x + epsilon) - func(x)) / epsilon
 
+def find_extr(func, a, b, epsilon, max=False, min=False):
+    a_0 = a
+    b_0 = b
+    while b - a > epsilon:
+        x_1 = b - (b - a) / golden
+        x_2 = a + (b - a) / golden
+        if max:
+            if func(x_1) <= func(x_2):
+                a = x_1
+            else:
+                b = x_2
+        if min:
+            if func(x_1) >= func(x_2):
+                a = x_1
+            else:
+                b = x_2
+    return builtins.max(func(a_0), func(b_0), func((a + b) / 2)) if max else builtins.min(func(a_0), func(b_0), func((a + b) / 2))
 
-def fixed_point_iteration(func, a, epsilon, newton=False, sor=False):
-    sgn = 1 if derivative(func, a, 10**(-9)) > 0 else -1
+def sor(func, x_0, a, b, epsilon):
+    cnt = 0
+    cnt_max = 10 ** 9
+    # g(x) = x - sgn * func(x); x_{i+1} = g(x_{i})
+    try:
+        df = lambda x: derivative(func, x, epsilon=10 ** (-9))
+        maximum = find_extr(df, a, b, epsilon=10**(-9), max=True)
+        minimum = find_extr(df, a, b, epsilon=10**(-9), min=True)
+        tau = 2 / (maximum + minimum)
+        g = lambda x: x - tau*func(x)
+    except OverflowError:
+        return "Method doesn't converge (overflow error)"
+
+    x_curr = g(x_0)
+
+    try:
+        while abs(x_curr - g(x_curr)) > epsilon and cnt < cnt_max:
+            x_curr = g(x_curr)
+            cnt += 1
+            print(x_curr)
+    except OverflowError:
+        return "Method doesn't converge (overflow error)"
+
+    if cnt >= cnt_max:
+        return "Method doesn't converge"
+
+    return f"\nSolution: {x_curr}\nIterations: {cnt}"
+
+def fixed_point_iteration(func, x_0, epsilon, newton=False):
+    sgn = 1 if derivative(func, x_0, 10**(-9)) > 0 else -1
     cnt = 0
     cnt_max = 10**9
 
@@ -33,13 +80,17 @@ def fixed_point_iteration(func, a, epsilon, newton=False, sor=False):
         if newton:
             g = lambda x: x - (1 / derivative(func, x, 10**(-9))) * func(x)
         elif sor:
-            g = lambda x: x - (1 / derivative(func, a, 10 ** (-9))) * func(x) * 2
+            df = lambda x: derivative(func, x, epsilon=10 ** (-9))
+            maximum = find_extr(df, a, b, epsilon=10**(-9), max=True)
+            minimum = find_extr(df, a, b, epsilon=10**(-9), min=True)
+            tau = 2 / (maximum + minimum)
+            g = lambda x: x - tau*func(x)
         else:
-            g = lambda x: x - (1 / derivative(func, a, 10 ** (-9))) * func(x)
+            g = lambda x: x - (1 / derivative(func, x_0, 10 ** (-9))) * func(x)
     except OverflowError:
         return "Method doesn't converge (overflow error)"
 
-    x_curr = g(a)
+    x_curr = g(x_0)
 
     try:
         while abs(x_curr - g(x_curr)) > epsilon and cnt < cnt_max:
@@ -62,7 +113,10 @@ def fixed_point_iteration(func, a, epsilon, newton=False, sor=False):
 # print(fixed_point_iteration(lambda x: x**3, 1.3, epsilon=10**(-9)))
 
 # newton method
-# print(fixed_point_iteration(lambda x: math.cos(x) - x**3, 1/2, epsilon=10**(-9), newton=True))
+# print(fixed_point_iteration(lambda x: math.cos(x) - x**3, 1/2,  epsilon=10**(-9), newton=True))
 
 # sor
-# print(fixed_point_iteration(lambda x: x**3 - 8 + math.sin(x), 50, epsilon=10**(-9), sor=True))
+# print(sor(lambda x: x**3 - 8 + math.sin(x), 10, -10, 5, epsilon=10**(-9)))
+
+# extr check
+# print(find_extr(lambda x: x**2 - 3*x**4, -1, 0, epsilon=10**(-9), max=True))
